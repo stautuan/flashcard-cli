1dc59c39275b9773ce8c65778bbeffa6d9f843d4
stautuan <stautuan89@gmail.com>
2024-07-28 19:45:52 -0400

    view entries

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import time
 
 def main():
     choice = ""
-    while choice != "2":
+    while choice != "3":
         clear_screen()
         print("What would you like to do?")
         print("1 - Add an entry")
-        print("2 - Quit\n")
+        print("2 - View entries")
+        print("3 - Quit\n")
         choice = input(">>> ")
 
         if choice == "1":
             add_entry()
         elif choice == "2":
+            get_entries()
+        elif choice == "3":
             print("Au revoir!")
             break
         else:
@@ -30,13 +33,25 @@ def add_entry():
     while True:
         clear_screen()
         print("======= New Entry ========")
-        sentence = input("Sentence: ")
-        definition = input("Definition: ")
-        list_dict[sentence] = definition
-        print("Added!\n")
+        with open("french.txt", "a") as f:
+            sentence = f.write(input("Sentence: "))
+            definition = f.write(input("Definition: "))
+            list_dict[sentence] = definition
+            print("Added!\n")
 
         choice = input("Continue? [y/n] ").lower()
         if choice == "n":
+            break
+
+
+def get_entries():
+    clear_screen()
+    f = open("french.txt", "r")
+    print(f.read())
+
+    while True:
+        choice = input("Press 1 to go back\n>>> ")
+        if choice == "1":
             break
 
 
